677b21d4458ab58c244ec6e3022c225aea10641b
JulianSchwaiger <55577397+JulianSchwaiger@users.noreply.github.com>
2021-12-22 08:43:06 +0100

    Swapped from python-twitter to tweepy
    :fire:

--- src/twitter_communications.py
+++ src/twitter_communications.py
@@ -1,14 +1,14 @@
-from twitter import Api
+import tweepy
 from loguru import logger
 
 
 def __init__(api_key, api_secret, access_token, access_secret) :
-    global api
+    global client
     try :
-        api = Api(  api_key,
-                    api_secret,
-                    access_token,
-                    access_secret)
-        logger.debug('Followers: ', api.GetFollowers())
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_secret)
+
+        client = tweepy.API(auth)
+        client.update_status("#Python Rocks!")
     except:
         logger.error('Unable to connect to twitter!')
