bcebcb7a99264317362633301fd32c24fcf888dc
DanielEizenberger <daniel0eizenberger@gmail.com>
2021-12-24 15:48:33 +0100

    Added basic communication with firestore
    Added functions to:
    - Fetch a tweet
    - Fetch a tweet and set it's "HasBeenTweeted" to true
    - Add a tweet to the database

--- src/backend.py
+++ src/backend.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from loguru import logger
+from datetime import datetime
 from firebase_admin import firestore
 from firebase_admin import credentials
 
@@ -12,13 +13,40 @@ def __init__(firebase_file_path) :
         db = firestore.client()
         tweets_ref = db.collection('Tweets')
         logger.info('Connected to firebase!')
-        printDocs()
     except :
         logger.error('Unable to connect to firebase!') 
 
-def printDocs() :
-    query = tweets_ref.where(u'HasBeenTweeted', u'==', False)#.limit(1)
+# Fetch a tweet
+def lookUpTweet() :
+    query = tweets_ref.where(u'HasBeenTweeted', u'==', False).limit(1)
     docs = query.stream()
 
-    for doc in docs:
-        logger.debug('{} => {} '.format(doc.id, doc.to_dict()))
+    for doc in docs :
+        return doc
+    
+    return None
+
+# Fetch a tweet and set it's "HasBeenTweeted" to true
+def useTweet() :
+    tweet = lookUpTweet()
+    
+    if tweet == None :
+        return None
+    
+    tweets_ref.document(tweet.id).update({u'HasBeenTweeted' : True})
+
+    return tweet
+
+# Add a tweet to the database
+def addTweet(text, username) :
+    tweet = {
+        u'HasBeenTweeted': False,
+        u'Time': datetime.utcnow(),
+        u'Text': text,
+        u'Username': username
+    }
+
+    docId = tweets_ref.document().id
+    tweets_ref.document(docId).set(tweet)
+
+    return tweet
