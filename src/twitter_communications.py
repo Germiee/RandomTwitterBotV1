ba27e99f7961ddb9c5aa4f1440f8174e1f77c07b
Julian Schwaiger <Julian.m.schwaiger@gmail.com>
2022-04-04 15:24:31 +0200

    added getMessage implementation

--- src/twitter_communications.py
+++ src/twitter_communications.py
@@ -12,5 +12,15 @@ def __init__(api_key, api_secret, access_token, access_secret) :
         client.update_status("#Python Rocks!")
         
         logger.info('Connected to twitter!')
+
+        get_new_messages()
     except:
         logger.error('Unable to connect to twitter!')
+
+def get_new_messages() :
+    toReturn = []
+    cursor = -1
+    messages = client.get_direct_messages(50, cursor)
+
+    for m in messages:
+        logger.info(m.message_create.message_data.text)
