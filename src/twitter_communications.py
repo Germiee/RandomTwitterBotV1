0116c1db0f149d64c76eb2317e566033a4f12a08
JulianSchwaiger <Julian.m.schwaiger@gmail.com>
2022-06-25 09:07:10 +0200

    more logging
    better logging for errors and connection to api

--- src/twitter_communications.py
+++ src/twitter_communications.py
@@ -9,8 +9,12 @@ def __init__(api_key, api_secret, access_token, access_secret) :
         auth.set_access_token(access_token, access_secret)
 
         client = tweepy.API(auth)
+        logger.info("################# - auth worked")
+
         client.update_status("#Python Rocks!")
-        
+
+        logger.info("################# - update_status worked")
+
         logger.info('Connected to twitter!')
 
         get_new_messages()
@@ -19,9 +23,9 @@ def __init__(api_key, api_secret, access_token, access_secret) :
         # but if you insist on printing message specifically whenever possible...
         logger.error('Unable to connect to twitter!')
         if hasattr(e, 'message'):
-            print(e.message)
+            logger.error("Error message: " +e.message)
         else:
-            print(e) 
+            logger.error("Error message: " +e) 
 
 def get_new_messages() :
     toReturn = []
@@ -30,3 +34,5 @@ def get_new_messages() :
 
     for m in messages:
         logger.info(m.message_create.message_data.text)
+
+        
